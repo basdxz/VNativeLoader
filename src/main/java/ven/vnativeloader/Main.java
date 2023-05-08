c85f82fc7d316afc22babc721ff08734938182af
Ven <tudurap.com@gmail.com>
2023-05-08 08:57:44 +0100

    Init

---
+++ src/main/java/ven/vnativeloader/Main.java
@@ -0,0 +1,11 @@
+package ven.vnativeloader;
+
+
+import lombok.*;
+
+public class Main {
+    public static void main(String[] args) {
+        val lombokTest = "Hello world!";
+        System.out.println(lombokTest);
+    }
+}
